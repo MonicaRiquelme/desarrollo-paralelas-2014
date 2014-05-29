8960e9c2f7d8a2f364d4967af5ec516ec59fdb88
marrubi <mar.arraubi@gmail.com>
2014-05-29 12:42:25 -0400

    Update pdfatxt.py

--- pdfatxt.py
+++ pdfatxt.py
@@ -11,7 +11,7 @@ comando = commands.getoutput("pdftotext "+sys.argv[1])
 txt= sys.argv[1].replace(".pdf", ".txt");
 
 string = open(txt).read()
-#eliminams todo menos letras
+#se reemplazan las letras con acento a letras sin acento
 str2 = string.replace("á","a").replace("é","e").replace("í","i").replace("ó","o").replace("ú","u").replace("Á","a").replace("É","e").replace("Í","i").replace("Ó","o").replace("Ú","u")
 new_str = re.sub('[^a-zA-Z]', '', str2)
 #todo a minusculas
