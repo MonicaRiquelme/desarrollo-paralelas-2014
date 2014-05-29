d313fb7372c3fe355d1934edb20e4fd0b8c78bfc
marrubi <mar.arraubi@gmail.com>
2014-05-29 12:41:55 -0400

    Reemplazo de código perl
    Se elimina código Perl, se reemplaza por .replace()

--- docatxt.py
+++ docatxt.py
@@ -7,24 +7,11 @@ import re
 print "Transformando de doc a txt... " + sys.argv[1]
 #Pasamos el .doc como argumento.
 txt= sys.argv[1].replace(".doc", ".txt");
-#eliminamos tildes de minusculas y mayusculas y la ñ
-comando = commands.getoutput("catdoc "+sys.argv[1]+" > "+txt)
-comando = commands.getoutput("perl -pi -e 's[á][a]g' "+txt)
-comando = commands.getoutput("perl -pi -e 's[é][e]g' "+txt)
-comando = commands.getoutput("perl -pi -e 's[í][i]g' "+txt)
-comando = commands.getoutput("perl -pi -e 's[ó][o]g' "+txt)
-comando = commands.getoutput("perl -pi -e 's[ú][u]g' "+txt)
-comando = commands.getoutput("perl -pi -e 's[Á][a]g' "+txt)
-comando = commands.getoutput("perl -pi -e 's[É][e]g' "+txt)
-comando = commands.getoutput("perl -pi -e 's[Í][i]g' "+txt)
-comando = commands.getoutput("perl -pi -e 's[Ó][o]g' "+txt)
-comando = commands.getoutput("perl -pi -e 's[Ú][u]g' "+txt)
-comando = commands.getoutput("perl -pi -e 's[ñ][n]g' "+txt)
-comando = commands.getoutput("perl -pi -e 's[Ñ][n]g' "+txt)
 
 string = open(txt).read()
-#eliminamos todo menos letras minusculas y mayusculas
-new_str = re.sub('[^a-zA-Z]', '', string)
+#reemplazo de las letras en la cadena string y guardado en str2
+str2 = string.replace("á","a").replace("é","e").replace("í","i").replace("ó","o").replace("ú","u").replace("Á","a").replace("É","e").replace("Í","i").replace("Ó","o").replace("Ú","u")
+new_str = re.sub('[^a-zA-Z]', '', str2)
 #dejamos todo en minusculas
 str = new_str.lower()
 open(txt, 'w').write(str)
